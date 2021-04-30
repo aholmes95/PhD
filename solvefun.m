d975223499c6fdbc8ea4f47dc0d28d5adda12d2f
aholmes95 <holmesa95@gmail.com>
2021-04-30 16:54:56 +0100

    Delete solvefun.m

--- solvefun.m
+++
@@ -1,7 +0,0 @@
-function kldiv = solvefun(a,e,b,g,N)
-epsSS = [g^2/((e+g)^2+b*e), 2*g*e/((e+g)^2+b*e), (e^2+b*e)/((e+g)^2+b*e)];
-retDist = findSSDistME(a,b,g,N);
-kldiv = abs(CalKLDiv(epsSS,retDist));
-% kldiv = sum(abs(epsSS-retDist));
-end
-
